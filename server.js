20bc6b984688412079cad0bc64250f7e85d41eb4
Ruslan Nugmanov <ruslan-nugmanov87@mail.ru>
2020-04-18 17:51:26 +0500

    3 version

--- server.js
+++ server.js
@@ -9,25 +9,33 @@ const app = new Koa();
 app.use(cors());
 app.use(koaBody({json: true}));
 
-const notes = [];
+let posts = [];
 let nextId = 1;
 
 const router = new Router();
 
 router.get('/posts', async (ctx, next) => {
-    ctx.response.body = notes;
+    ctx.response.body = posts;
 });
 
 router.post('/posts', async(ctx, next) => {
-    notes.push({...ctx.request.body, id: nextId++});
+    const {id, content} = ctx.request.body;
+
+    if (id !== 0) {
+        posts = posts.map(o => o.id !== id ? o : {...o, content: content});
+        ctx.response.status = 204;
+        return;
+    }
+
+    posts.push({...ctx.request.body, id: nextId++, created: Date.now()});
     ctx.response.status = 204;
 });
 
 router.delete('/posts/:id', async(ctx, next) => {
-    const noteId = Number(ctx.params.id);
-    const index = notes.findIndex(o => o.id === noteId);
+    const postId = Number(ctx.params.id);
+    const index = posts.findIndex(o => o.id === postId);
     if (index !== -1) {
-        notes.splice(index, 1);
+        posts.splice(index, 1);
     }
     ctx.response.status = 204;
 });
